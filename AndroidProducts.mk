a0e9ff62547d6f99572a5404daa37ca27ed69d48
alien-13 <nathan.belci@gmail.com>
2015-07-14 17:29:38 +0930

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
- PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_m7.mk \
-$(LOCAL_DIR)/full_m7.mk \
-$(LOCAL_DIR)/omni_m7.mk
-=======
 PRODUCT_MAKEFILES := \
   $(LOCAL_DIR)/device_m7.mk \
   $(LOCAL_DIR)/full_m7.mk \
   $(LOCAL_DIR)/omni_m7.mk
->>>>>>> b967575a573bf2994912bf48ab2793cb0e997580
